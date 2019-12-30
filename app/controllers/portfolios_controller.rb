c774600085351bc9f733e5192a0d6ea914b46c22
xwar <marek.rawski@gmail.com>
2019-12-30 18:32:15 +0100

    Added new and edit buttons for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,24 +1,46 @@
 class PortfoliosController < ApplicationController
 	def index
-		@portfolio_items = Portfolio.all
+		@portfolio_item = Portfolio.all
 	end
 
 	def new
-		@portfolio_items = Portfolio.new
+		@portfolio_item = Portfolio.new
 	end
 
-  def create
-    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+  	def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    	respond_to do |format|
+      		if @portfolio_item.save
+		        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+		        format.json { render :show, status: :created, location: @portfolio_item }
+		     else
+		        format.html { render :new }
+		        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+		    end
+    	end
+  	end
+
+
+
+
+  def edit
+  	@portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+	@portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_items.save
-        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
-        format.json { render :show, status: :created, location: @portfolio_items }
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+        #format.json { render :show, status: :ok, location: @portfolio_item }
       else
-        format.html { render :new }
-        format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+        format.html { render :edit }
+        #format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
   end
+
 
 end
